ca2a8d4944095db91799edc0d3fbd117aacf1f8c
Zao Soula <contact@zaosoula.fr>
2020-03-03 13:15:42 +0100

    V1.0.0

---
+++ README.md
@@ -0,0 +1,124 @@
+# Deezer Public API wrapper
+
+This is *Nodejs* wrapper for the [Deezer Public API](http://developers.deezer.com/api) that return promises.
+
+## Installation
+    npm install deezer-open-api --save
+
+## Usage example
+
+```js
+const DeezerOpenApi = require('deezer-open-api');
+let deezer = new DeezerOpenApi();
+
+//Search an artist
+deezer.search.artist('ILYSH').then(function(result) {
+   console.log(result);
+});
+
+//Get info for the given artist id
+deezer.artist('58671252').then(function(result) {
+   console.log(result);
+});
+
+//Get album list for the given artist id
+deezer.artist.albums('58671252').then(function(result) {
+   console.log(result);
+});
+```
+## Available methods
+
+##### Album
+* album(`id`, `limit`, `index`)
+* album.comments(`id`, `limit`, `index`)
+* album.fans(`id`, `limit`, `index`)
+* album.tracks(`id`, `limit`, `index`)
+
+##### Artist
+* artist(`id`, `limit`, `index`)
+* artist.top(`id`, `limit`, `index`)
+* artist.albums(`id`, `limit`, `index`)
+* artist.comments(`id`, `limit`, `index`)
+* artist.fans(`id`, `limit`, `index`)
+* artist.related(`id`, `limit`, `index`)
+* artist.radio(`id`, `limit`, `index`)
+* artist.playlists(`id`, `limit`, `index`)
+
+##### Chart
+* chart(`limit`, `index`)
+* chart.tracks(`limit`, `index`)
+* chart.albums(`limit`, `index`)
+* chart.artists(`limit`, `index`)
+* chart.playlists(`limit`, `index`)
+* chart.podcasts(`limit`, `index`)
+
+##### Comment
+* comment(`id`, `limit`, `index`)
+
+##### Editorial
+* editorial(`id`, `limit`, `index`)
+* editorial.selection(`id`, `limit`, `index`)
+* editorial.charts(`id`, `limit`, `index`)
+* editorial.releases(`id`, `limit`, `index`)
+
+##### Episode
+* episode(`id`, `limit`, `index`)
+
+##### Genre
+* genre(`id`, `limit`, `index`)
+* genre.artists(`id`, `limit`, `index`)
+* genre.podcasts(`id`, `limit`, `index`)
+* genre.radios(`id`, `limit`, `index`)
+
+##### Infos
+* infos()
+
+##### Options
+* options()
+
+##### Playlist
+* playlist(`id`, `limit`, `index`)
+* playlist.comments(`id`, `limit`, `index`)
+* playlist.fans(`id`, `limit`, `index`)
+* playlist.tracks(`id`, `limit`, `index`)
+* playlist.radio(`id`, `limit`, `index`)
+
+##### Podcast
+* podcast(`id`, `limit`, `index`)
+* podcast.episodes(`id`, `limit`, `index`)
+
+##### Radio
+* radio(`id`, `limit`, `index`)
+* radio.genres(`limit`, `index`)
+* radio.top(`limit`, `index`)
+* radio.lists(`limit`, `index`)
+* radio.tracks(`id`, `limit`, `index`)
+
+##### Track
+* track(`id`, `limit`, `index`)
+
+##### User
+* user(`id`, `limit`, `index`)
+* user.albums(`id`, `limit`, `index`)
+* user.artists(`id`, `limit`, `index`)
+* user.charts(`id`, `limit`, `index`)
+* user.charts.tracks(`id`, `limit`, `index`)
+* user.charts.albums(`id`, `limit`, `index`)
+* user.charts.playlists(`id`, `limit`, `index`)
+* user.charts.artists(`id`, `limit`, `index`)
+* user.flow(`id`, `limit`, `index`)
+* user.followings(`id`, `limit`, `index`)
+* user.followers(`id`, `limit`, `index`)
+* user.playlists(`id`, `limit`, `index`)
+* user.podcasts(`id`, `limit`, `index`)
+* user.radios(`id`, `limit`, `index`)
+* user.tracks(`id`, `limit`, `index`)
+
+##### Search
+* search(`options`, `order`, `limit`, `index`)
+* search.artist(`query`, `order`, `limit`, `index`, `strict`)
+* search.playlist(`query`, `order`, `limit`, `index`, `strict`)
+* search.podcast(`query`, `order`, `limit`, `index`, `strict`)
+* search.radio(`query`, `order`, `limit`, `index`, `strict`)
+* search.track(`query`, `order`, `limit`, `index`, `strict`)
+* search.user(`query`, `order`, `limit`, `index`, `strict`)
--- src/index.js
+++ src/index.js
@@ -1,102 +1,105 @@
 var rp = require('request-promise');
 const apiUrl = 'https://api.deezer.com/'
 
-function DeezerOpenApi() {
+function DeezerPublicApi() {
   this.apiUrl = apiUrl;
 }
+
 /*
  *  ALBUM
  */
-DeezerOpenApi.prototype.album = function(id, limit, index) {
+DeezerPublicApi.prototype.album = function(id, limit, index) {
   var url = 'album/' + id;
   return rq(url, index, limit);
 };
 
-DeezerOpenApi.prototype.album.comments = function(id, limit, index) {
+DeezerPublicApi.prototype.album.comments = function(id, limit, index) {
   var url = 'album/' + id + '/comments';
   return rq(url, index, limit);
 };
 
-DeezerOpenApi.prototype.album.fans = function(id, limit, index) {
+DeezerPublicApi.prototype.album.fans = function(id, limit, index) {
   var url = 'album/' + id + '/fans';
   return rq(url, index, limit);
 };
 
-DeezerOpenApi.prototype.album.tracks = function(id, limit, index) {
+DeezerPublicApi.prototype.album.tracks = function(id, limit, index) {
   var url = 'album/' + id + '/tracks';
   return rq(url, index, limit);
 };
+
 /*
  *  ARTIST
  */
-DeezerOpenApi.prototype.artist = function(id, limit, index) {
+DeezerPublicApi.prototype.artist = function(id, limit, index) {
   var url = 'artist/' + id;
   return rq(url, index, limit);
 };
 
-DeezerOpenApi.prototype.artist.top = function(id, limit, index) {
+DeezerPublicApi.prototype.artist.top = function(id, limit, index) {
   var url = 'artist/' + id + '/top';
   return rq(url, index, limit);
 };
 
-DeezerOpenApi.prototype.artist.albums = function(id, limit, index) {
+DeezerPublicApi.prototype.artist.albums = function(id, limit, index) {
   var url = 'artist/' + id + '/albums';
   return rq(url, index, limit);
 };
 
-DeezerOpenApi.prototype.artist.comments = function(id, limit, index) {
+DeezerPublicApi.prototype.artist.comments = function(id, limit, index) {
   var url = 'artist/' + id + '/comments';
   return rq(url, index, limit);
 };
 
-DeezerOpenApi.prototype.artist.fans = function(id, limit, index) {
+DeezerPublicApi.prototype.artist.fans = function(id, limit, index) {
   var url = 'artist/' + id + '/fans';
   return rq(url, index, limit);
 };
 
-DeezerOpenApi.prototype.artist.related = function(id, limit, index) {
+DeezerPublicApi.prototype.artist.related = function(id, limit, index) {
   var url = 'artist/' + id + '/related';
   return rq(url, index, limit);
 };
 
-DeezerOpenApi.prototype.artist.radio = function(id, limit, index) {
+DeezerPublicApi.prototype.artist.radio = function(id, limit, index) {
   var url = 'artist/' + id + '/radio';
   return rq(url, index, limit);
 };
 
-DeezerOpenApi.prototype.artist.playlists = function(id, limit, index) {
+DeezerPublicApi.prototype.artist.playlists = function(id, limit, index) {
   var url = 'artist/' + id + '/playlists';
   return rq(url, index, limit);
 };
+
 /*
  *  CHART
  */
-DeezerOpenApi.prototype.chart = function(limit, index) {
+DeezerPublicApi.prototype.chart = function(limit, index) {
   var url = 'chart/';
   return rq(url, index, limit);
 };
 
-DeezerOpenApi.prototype.chart.tracks = function(limit, index) {
+DeezerPublicApi.prototype.chart.tracks = function(limit, index) {
   var url = 'chart/0/tracks';
   return rq(url, index, limit);
 };
 
-DeezerOpenApi.prototype.chart.albums = function(limit, index) {
+DeezerPublicApi.prototype.chart.albums = function(limit, index) {
   var url = 'chart/0/albums';
   return rq(url, index, limit);
 };
 
-DeezerOpenApi.prototype.chart.artists = function(limit, index) {
+DeezerPublicApi.prototype.chart.artists = function(limit, index) {
   var url = 'chart/0/artists';
   return rq(url, index, limit);
 };
 
-DeezerOpenApi.prototype.chart.playlists = function(limit, index) {
+DeezerPublicApi.prototype.chart.playlists = function(limit, index) {
   var url = 'chart/0/playlists';
   return rq(url, index, limit);
 };
 
-DeezerOpenApi.prototype.chart.podcasts = function(limit, index) {
+DeezerPublicApi.prototype.chart.podcasts = function(limit, index) {
   var url = 'chart/0/podcasts';
   return rq(url, index, limit);
 };
@@ -104,230 +107,243 @@ DeezerOpenApi.prototype.chart.podcasts = function(limit, index) {
 /*
  *  COMMENT
  */
-DeezerOpenApi.prototype.comment = function(id, limit, index) {
+DeezerPublicApi.prototype.comment = function(id, limit, index) {
   var url = 'comment/'+id;
   return rq(url, index, limit);
 };
+
 /*
  *  EDITORIAL
  */
-DeezerOpenApi.prototype.editorial = function(id, limit, index) {
+DeezerPublicApi.prototype.editorial = function(id, limit, index) {
   var url = 'editorial/'+((id)?id:'');
   return rq(url, index, limit);
 };
 
-DeezerOpenApi.prototype.editorial.selection = function(id, date, limit, index) {
+DeezerPublicApi.prototype.editorial.selection = function(id, date, limit, index) {
   var url = 'editorial/' + id + '/selection';
   if (date) url = url + '&date=' + date;
   return rq(url, index, limit);
 };
 
-DeezerOpenApi.prototype.editorial.charts = function(id, limit, index) {
+DeezerPublicApi.prototype.editorial.charts = function(id, limit, index) {
   var url = 'editorial/' + id + '/charts';
   return rq(url, index, limit);
 };
 
-DeezerOpenApi.prototype.editorial.releases = function(id, limit, index) {
+DeezerPublicApi.prototype.editorial.releases = function(id, limit, index) {
   var url = 'editorial/' + id + '/releases';
   return rq(url, index, limit);
 };
+
 /*
  *  EPISODE
  */
-DeezerOpenApi.prototype.episode = function(id, limit, index) {
+DeezerPublicApi.prototype.episode = function(id, limit, index) {
   var url = 'episode/'+id;
   return rq(url, index, limit);
 };
+
 /*
  *  GENRE
  */
-DeezerOpenApi.prototype.genre = function(id, limit, index) {
+DeezerPublicApi.prototype.genre = function(id, limit, index) {
   var url = 'genre/'+((id)?id:'');
   return rq(url, index, limit);
 };
 
-DeezerOpenApi.prototype.genre.artists = function(id, limit, index) {
+DeezerPublicApi.prototype.genre.artists = function(id, limit, index) {
   var url = 'genre/' + id + '/artists';
   return rq(url, index, limit);
 };
 
-DeezerOpenApi.prototype.genre.podcasts = function(id, limit, index) {
+DeezerPublicApi.prototype.genre.podcasts = function(id, limit, index) {
   var url = 'genre/' + id + '/podcasts';
   return rq(url, index, limit);
 };
 
-DeezerOpenApi.prototype.genre.radios = function(id, limit, index) {
+DeezerPublicApi.prototype.genre.radios = function(id, limit, index) {
   var url = 'genre/' + id + '/radios';
   return rq(url, index, limit);
 };
+
 /*
  *  INFOS
  */
-DeezerOpenApi.prototype.infos = function() {
+DeezerPublicApi.prototype.infos = function() {
   var url = 'infos';
   return rq(url);
 };
+
 /*
  *  OPTIONS
  */
-DeezerOpenApi.prototype.options = function() {
+DeezerPublicApi.prototype.options = function() {
   var url = 'options';
   return rq(url);
 };
+
 /*
  *  PLAYLIST
  */
-DeezerOpenApi.prototype.playlist = function(id, limit, index) {
+DeezerPublicApi.prototype.playlist = function(id, limit, index) {
   var url = 'playlist/' + id;
   return rq(url, index, limit);
 };
 
-DeezerOpenApi.prototype.playlist.comments = function(id, limit, index) {
+DeezerPublicApi.prototype.playlist.comments = function(id, limit, index) {
   var url = 'playlist/' + id + '/comments';
   return rq(url, index, limit);
 };
 
-DeezerOpenApi.prototype.playlist.fans = function(id, limit, index) {
+DeezerPublicApi.prototype.playlist.fans = function(id, limit, index) {
   var url = 'playlist/' + id + '/fans';
   return rq(url, index, limit);
 };
 
-DeezerOpenApi.prototype.playlist.tracks = function(id, limit, index) {
+DeezerPublicApi.prototype.playlist.tracks = function(id, limit, index) {
   var url = 'playlist/' + id + '/tracks';
   return rq(url, index, limit);
 };
 
-DeezerOpenApi.prototype.playlist.radio = function(id, limit, index) {
+DeezerPublicApi.prototype.playlist.radio = function(id, limit, index) {
   var url = 'playlist/' + id + '/radio';
   return rq(url, index, limit);
 };
+
 /*
  *  PODCAST
  */
-DeezerOpenApi.prototype.podcast = function(id, limit, index) {
+DeezerPublicApi.prototype.podcast = function(id, limit, index) {
   var url = 'podcast/' + ((id)?id:'');
   return rq(url, index, limit);
 };
 
-DeezerOpenApi.prototype.podcast.episodes = function(id, limit, index) {
+DeezerPublicApi.prototype.podcast.episodes = function(id, limit, index) {
   var url = 'podcast/' + id + '/episodes';
   return rq(url, index, limit);
 };
+
 /*
  *  RADIO
  */
-DeezerOpenApi.prototype.radio = function(id, limit, index) {
+DeezerPublicApi.prototype.radio = function(id, limit, index) {
   var url = 'radio/' + ((id)?id:'');
   return rq(url, index, limit);
 };
 
-DeezerOpenApi.prototype.radio.genres = function(id, limit, index) {
+DeezerPublicApi.prototype.radio.genres = function(limit, index) {
   var url = 'radio/genres';
   return rq(url, index, limit);
 };
 
-DeezerOpenApi.prototype.radio.top = function(id, limit, index) {
+DeezerPublicApi.prototype.radio.top = function(limit, index) {
   var url = 'radio/top';
   return rq(url, index, limit);
 };
 
-DeezerOpenApi.prototype.radio.lists = function(id, limit, index) {
+DeezerPublicApi.prototype.radio.lists = function(limit, index) {
   var url = 'radio/lists';
   return rq(url, index, limit);
 };
 
-DeezerOpenApi.prototype.radio.tracks = function(id, limit, index) {
+DeezerPublicApi.prototype.radio.tracks = function(id, limit, index) {
   var url = 'radio/' + id + '/tracks';
   return rq(url, index, limit);
 };
+
 /*
  *  TRACK
  */
-DeezerOpenApi.prototype.track = function(id, limit, index) {
+DeezerPublicApi.prototype.track = function(id, limit, index) {
   var url = 'track/'+id;
   return rq(url, index, limit);
 };
+
 /*
  *  USER
  */
-DeezerOpenApi.prototype.user = function(id, limit, index) {
+DeezerPublicApi.prototype.user = function(id, limit, index) {
   var url = 'user/'+id;
   return rq(url, index, limit);
 };
 
-DeezerOpenApi.prototype.user.albums = function(id, limit, index) {
+DeezerPublicApi.prototype.user.albums = function(id, limit, index) {
   var url = 'user/' + id + '/albums';
   return rq(url, index, limit);
 };
 
-DeezerOpenApi.prototype.user.artists = function(id, limit, index) {
+DeezerPublicApi.prototype.user.artists = function(id, limit, index) {
   var url = 'user/' + id + '/artists';
   return rq(url, index, limit);
 };
 
-DeezerOpenApi.prototype.user.charts = function(id, limit, index) {
+DeezerPublicApi.prototype.user.charts = function(id, limit, index) {
   var url = 'user/' + id + '/charts';
   return rq(url, index, limit);
 };
 
-DeezerOpenApi.prototype.user.charts.tracks = function(id, limit, index) {
+DeezerPublicApi.prototype.user.charts.tracks = function(id, limit, index) {
   var url = 'user/' + id + '/charts/tracks';
   return rq(url, index, limit);
 };
 
-DeezerOpenApi.prototype.user.charts.albums = function(id, limit, index) {
+DeezerPublicApi.prototype.user.charts.albums = function(id, limit, index) {
   var url = 'user/' + id + '/charts/albums';
   return rq(url, index, limit);
 };
 
-DeezerOpenApi.prototype.user.charts.playlists = function(id, limit, index) {
+DeezerPublicApi.prototype.user.charts.playlists = function(id, limit, index) {
   var url = 'user/' + id + '/charts/playlists';
   return rq(url, index, limit);
 };
 
-DeezerOpenApi.prototype.user.charts.artists = function(id, limit, index) {
+DeezerPublicApi.prototype.user.charts.artists = function(id, limit, index) {
   var url = 'user/' + id + '/charts/artists';
   return rq(url, index, limit);
 };
 
-DeezerOpenApi.prototype.user.flow = function(id, limit, index) {
+DeezerPublicApi.prototype.user.flow = function(id, limit, index) {
   var url = 'user/' + id + '/flow';
   return rq(url, index, limit);
 };
 
-DeezerOpenApi.prototype.user.followings = function(id, limit, index) {
+DeezerPublicApi.prototype.user.followings = function(id, limit, index) {
   var url = 'user/' + id + '/followings';
   return rq(url, index, limit);
 };
 
-DeezerOpenApi.prototype.user.followers = function(id, limit, index) {
+DeezerPublicApi.prototype.user.followers = function(id, limit, index) {
   var url = 'user/' + id + '/followers';
   return rq(url, index, limit);
 };
 
-DeezerOpenApi.prototype.user.playlists = function(id, limit, index) {
+DeezerPublicApi.prototype.user.playlists = function(id, limit, index) {
   var url = 'user/' + id + '/playlists';
   return rq(url, index, limit);
 };
 
-DeezerOpenApi.prototype.user.podcasts = function(id, limit, index) {
+DeezerPublicApi.prototype.user.podcasts = function(id, limit, index) {
   var url = 'user/' + id + '/podcasts';
   return rq(url, index, limit);
 };
 
-DeezerOpenApi.prototype.user.radios = function(id, limit, index) {
+DeezerPublicApi.prototype.user.radios = function(id, limit, index) {
   var url = 'user/' + id + '/radios';
   return rq(url, index, limit);
 };
 
-DeezerOpenApi.prototype.user.tracks = function(id, limit, index) {
+DeezerPublicApi.prototype.user.tracks = function(id, limit, index) {
   var url = 'user/' + id + '/tracks';
   return rq(url, index, limit);
 };
+
+
+
 /*
  *  SEARCH
  */
-DeezerOpenApi.prototype.search = function(options, order, limit, index) {
+DeezerPublicApi.prototype.search = function(options, order, limit, index) {
   var url = 'search?q=';
   var query = '';
   if (typeof options === 'object') {
@@ -342,35 +358,36 @@ DeezerOpenApi.prototype.search = function(options, order, limit, index) {
   return rq(url, index, limit, order);
 };
 
-DeezerOpenApi.prototype.search.artist = function(query, order, limit, index, strict) {
+DeezerPublicApi.prototype.search.artist = function(query, order, limit, index, strict) {
   var url = 'search/artist?q=' + query;
   return rq(url, index, limit, order, strict);
 };
 
-DeezerOpenApi.prototype.search.playlist = function(query, order, limit, index, strict) {
+DeezerPublicApi.prototype.search.playlist = function(query, order, limit, index, strict) {
   var url = 'search/playlist?q=' + query;
   return rq(url, index, limit, order, strict);
 };
 
-DeezerOpenApi.prototype.search.podcast = function(query, order, limit, index, strict) {
+DeezerPublicApi.prototype.search.podcast = function(query, order, limit, index, strict) {
   var url = 'search/podcast?q=' + query;
   return rq(url, index, limit, order, strict);
 };
 
-DeezerOpenApi.prototype.search.radio = function(query, order, limit, index, strict) {
+DeezerPublicApi.prototype.search.radio = function(query, order, limit, index, strict) {
   var url = 'search/radio?q=' + query;
   return rq(url, index, limit, order, strict);
 };
 
-DeezerOpenApi.prototype.search.track = function(query, order, limit, index, strict) {
+DeezerPublicApi.prototype.search.track = function(query, order, limit, index, strict) {
   var url = 'search/track?q=' + query;
   return rq(url, index, limit, order, strict);
 };
 
-DeezerOpenApi.prototype.search.user = function(query, order, limit, index, strict) {
+DeezerPublicApi.prototype.search.user = function(query, order, limit, index, strict) {
   var url = 'search/user?q=' + query;
   return rq(url, index, limit, order, strict);
 };
+
 function rq(url, index, limit, order, strict){
   if(!url.includes("?")) url = url + '?';
   if (index && index !== 0) url = url + '&index=' + index;
@@ -384,4 +401,4 @@ function rq(url, index, limit, order, strict){
     json: true
   });
 }
-module.exports = DeezerOpenApi;
+module.exports = DeezerPublicApi;
